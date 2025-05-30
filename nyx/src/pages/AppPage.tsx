f24b121136a7a77f53721ddef60c2109f2cca99d
catellum424 <czingone31@gmail.com>
2025-05-30 22:01:54 +0200

    Delete nyx directory

--- nyx/src/pages/AppPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function AppPage() {
-  return (
-    <main style={{ padding: '2rem' }}>
-      <h2>Area operativa</h2>
-      <p>Qui puoi usare la tua IA o generare lettere.</p>
-    </main>
-  );
-}
