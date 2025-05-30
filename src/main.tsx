f0a277d7146996df7dc80083625537b8d91490a9
catellum424 <czingone31@gmail.com>
2025-05-30 22:03:37 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Plans from './pages/Plans'
+import AppPage from './pages/AppPage'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/plans" element={<Plans />} />
+        <Route path="/app" element={<AppPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/pages/AppPage.tsx
@@ -0,0 +1,8 @@
+export default function AppPage() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h2>Area operativa</h2>
+      <p>Qui puoi usare la tua IA o generare lettere.</p>
+    </main>
+  );
+}
